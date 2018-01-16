5b8d57f3841ef522a5e77d82ec0a8788a25ab463
Andrew Banks <andrewbanks10@gmail.com>
2018-01-16 12:10:21 -0500

    Add files via upload

--- src/react-components/StateMonitor/hmrchangedmodules.js
+++ src/react-components/StateMonitor/hmrchangedmodules.js
@@ -1,38 +1,19 @@
-/* eslint no-undef: 0 */
-/* eslint camelcase: 0 */
 import { handleHMRLoadedModules } from './controller'
 
-// Webpack particulars.
-const chunkId = 0
-const changedFileStart = '/***/'
-const changedFileEnd = ':'
-const hrmFileSuffix = '.hot-update.js'
-const hrmEvent = 'webpackHotUpdate'
-
-// From webpack, this is the url that contains the hmr changed code.
-const hrmPatchURL = () =>
-  __webpack_require__.p + '' + chunkId + '.' + __webpack_hash__ + hrmFileSuffix
-
-// This is used to determine what file(s) have been updated with HMR
+const webpackHotUpdate = 'webpackHotUpdate'
 if (module.hot) {
-  // Event sent on HMR.
-  require('webpack/hot/emitter').on(hrmEvent, () => {
-    // Get the patch from the webpack dev server
-    fetch(hrmPatchURL()).then(response =>
-      response.text()
-    ).then(data => {
-      try {
-        // Find the file that was changed in the hmr patch.
-        const start = data.indexOf(changedFileStart) + changedFileStart.length
-        const end = data.indexOf(changedFileEnd)
-        let changedFile = data.substring(start, end).trim().replace(/'|"/g, '')
-        // Record the changed file,
-        handleHMRLoadedModules([changedFile])
-      } catch (msg) {
-        throw new Error(`Hot patch parse failed ${msg}.`)
+  var parentHotUpdateCallback = window[webpackHotUpdate]
+  // Webpack uses window[webpackHotUpdate] as the function to call on a hmr.
+  // So, save that function and replace it with one of our own so we
+  // can determine which module source files changed. Then
+  // call the webpack function that was saved.
+  window[webpackHotUpdate] =
+    function webpackHotUpdateCallback (chunkId, moreModules) {
+      // Call the webpack hrm handler.
+      if (typeof parentHotUpdateCallback === 'function') {
+        parentHotUpdateCallback(chunkId, moreModules)
       }
-    }).catch(function () {
-      console.log(`fetch of ${hotURL} failed.`)
-    })
-  })
+      // Now handle the changed source modules.
+      handleHMRLoadedModules(Object.keys(moreModules))
+    }
 }
